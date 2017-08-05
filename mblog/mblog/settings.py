ea7befe1a088bae6bbaf2e0064a3cb1c50314757
liucw <yjwwyygy@163.com>
2017-08-05 02:30:47 -0700

    修改配置文件

--- mblog/mblog/settings.py
+++ mblog/mblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'go3!_)ge+$w%8d$^-(o@yt#z36+ak&vf^(a$wksc&=!dufo8m@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainsite',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
